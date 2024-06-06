bbdd15286149749638a0a4caafa7af30549b9324
Sebastian Andrade <sandradecedano@gmail.com>
2024-06-06 15:07:53 -0500

    Fixed some endpoints

--- src/routes/acompaniamientoIntegral.routes.js
+++ src/routes/acompaniamientoIntegral.routes.js
@@ -29,6 +29,24 @@ router.post('/citas-asesoria', async (req, res) => {
 
 router.delete('/citas-asesoria/:id', async (req, res) => {
     try {
+        const [eventResult] = await callProcedure(
+            req.session.user.username,
+            req.session.user.password,
+            "consultar_info_cita_asesoria_evento",
+            [req.params.id]
+        );
+
+        if(!eventResult[0][0]){
+            return res.status(404).json({message: `No appointments with id ${req.params.id}`});
+        }
+
+        if(req.session.user.role === "estudiante"){
+
+            if(eventResult[0][0].cit_ase_est_per_DNI !== parseInt(req.session.user.username)){
+                return res.status(403).json({message: "Not allowed to cancel someone elses appointment."});
+            }
+        }
+
         await callProcedure(
             req.session.user.username,
             req.session.user.password,
