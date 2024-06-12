a5e0ed54cf24596dcd8ab67111089e2f955665fa
Cristian Motta <cristianstevenmottaojeda@gmail.com>
2024-06-12 02:18:41 -0500

    Changed frontend url config

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 export const PORT = 4000;
-export const FRONTEND_URL = "http://localhost:3000";
+export const FRONTEND_URL = "http://localhost:5173";
 export const DB_HOST = "localhost";
 export const DB_PORT = 3306;
 export const DB_NAME = "bienestar_un";
