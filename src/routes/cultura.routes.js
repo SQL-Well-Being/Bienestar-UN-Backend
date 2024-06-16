a261c3aeb0c6a00a40bacb257fd92098f73b62ad
Cristian Motta <cristianstevenmottaojeda@gmail.com>
2024-06-16 00:40:41 -0500

    Added endpoints for cultura

--- src/routes/cultura.routes.js
+++ src/routes/cultura.routes.js
@@ -1,8 +1,104 @@
 import { Router } from "express";
 import callProcedure from "../libs/callProcedure.js";
+import onErrorResponse from "../libs/onErrorResponse.js";
 
 const router = Router();
 
-// Put endpoints here
+router.get("/gai", async (req, res) => {
+  try {
+    const [result] = await callProcedure(
+      req.user.username,
+      req.user.password,
+      "obtener_grupos_artisticos_institucionales"
+    );
+    res.json(result[0]);
+  } catch (e) {
+    onErrorResponse(res, e);
+  }
+});
+
+router.get("/talleres-culturales", async (req, res) => {
+  try {
+    const [result] = await callProcedure(
+      req.user.username,
+      req.user.password,
+      "obtener_talleres_culturales"
+    );
+    res.json(result[0]);
+  } catch (e) {
+    onErrorResponse(res, e);
+  }
+});
+
+router.get("/talleres-culturales/:id", async (req, res) => {
+  try {
+    const [result] = await callProcedure(
+      req.user.username,
+      req.user.password,
+      "obtener_taller_cultural",
+      [req.params.id]
+    );
+    res.json(result[0]);
+  } catch (e) {
+    onErrorResponse(res, e);
+  }
+});
+
+router.get("/convocatorias-gai", async (req, res) => {
+  try {
+    const [result] = await callProcedure(
+      req.user.username,
+      req.user.password,
+      "obtener_convocatorias_gai"
+    );
+    res.json(result);
+  } catch (e) {
+    onErrorResponse(res, e);
+  }
+});
+
+router.get("/gai/estudiantes/:id", async (req, res) => {
+  try {
+    const [result] = await callProcedure(
+      req.user.username,
+      req.user.password,
+      "obtener_estudiantes_pertenecen_gai",
+      [req.params.id]
+    );
+    res.json(result);
+  } catch (e) {
+    onErrorResponse(res, e);
+  }
+});
+
+router.get("/talleres-culturales/asistencia/:id", async (req, res) => {
+  try {
+    const [result] = await callProcedure(
+      req.user.username,
+      req.user.password,
+      "obtener_estudiantes_asistieron_taller_cultural",
+      [req.params.id]
+    );
+    res.json(result);
+  } catch (e) {
+    onErrorResponse(res, e);
+  }
+});
+
+router.post("/talleres-culturales/asistencia/:id", async (req, res) => {
+  try {
+    const { DNI } = req.body;
+
+    const [result] = await callProcedure(
+      req.user.username,
+      req.user.password,
+      "llenar_asistencia_taller_cultural",
+      [DNI, req.params.id]
+    );
+    res.json(result);
+  } catch (e) {
+    onErrorResponse(res, e);
+  }
+});
 
 export default router;
